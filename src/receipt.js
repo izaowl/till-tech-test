98dda36313a5dc191fb7882b47fb6edfe5cb8fdd
izaowl <izasow@yahoo.com>
2022-03-27 23:25:06 +0100

    basic set up for JSON file to be read

--- src/receipt.js
+++ src/receipt.js
@@ -12,12 +12,27 @@ class Receipt{
     try {
         const jsonString = fs.readFileSync('./src/hipstercoffee.json')
         const info = JSON.parse(jsonString)
-        for (var i=0; i<info.length; i++){
-        for (var shopDetails in info[i]) {
-            console.log(info[0].prices[0]['Cafe Latte']);
-            console.log(info[0][shopDetails]);
-        }
-    }
+        var currentdate = new Date(); 
+var datetime = "" + currentdate.getFullYear() + "."
+                + (currentdate.getMonth()+1)  + "." 
+                + currentdate.getDate() + " "
+                + currentdate.getHours() + ":"  
+                + currentdate.getMinutes() + ":" 
+                + currentdate.getSeconds();
+        console.log(datetime)
+        console.log(info[0].shopName);
+        console.log(info[0].address);
+        console.log("             ")
+        console.log(" + "+ info[0].phone);
+        console.log(info[0].prices[0]['Cafe Latte'])
+    //     for (var i=0; i<info.length; i++){
+    //     for (var shopDetails in info[i]) {
+    //         console.log(info[0].prices[0]['Cafe Latte']);
+            
+    //         console.log(info[0].shopName);
+    //         console.log(info[0][shopDetails]);
+    //     }
+    // }
         // for(var i = 0; i < info.length; ++i){
         //     //do something with obj[i]
         //     for(var ind in info[i]) {
